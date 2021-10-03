9737f61ae98ea3ca88fc1f5d04f40f324a43c997
Doug <douganderson444@gmail.com>
2021-10-03 17:29:38 -0300

    add adapter for deployment

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,10 +1,15 @@
 import preprocess from 'svelte-preprocess';
+import adapter_ipfs from 'sveltejs-adapter-ipfs';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 
 	kit: {
+		adapter: adapter_ipfs({
+			removeBuiltInServiceWorkerRegistration: true,
+			injectPagesInServiceWorker: true
+		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
