c75a2282553d0223aae0b2cbdd232eaf62ad16fb
Doug <douganderson444@gmail.com>
2021-10-07 12:35:24 -0300

    remove fs dependencies

--- app/src/lib/helpers/utils.ts
+++ app/src/lib/helpers/utils.ts
@@ -1,11 +1,8 @@
 import { selectedNetwork, anchorClient } from '$lib/stores';
 import { get } from 'svelte/store';
-import { programIdMainnet, DEV_NET, configMainnet } from '$lib/constants';
+import { programId, MAIN_NET, DEV_NET, config } from '$lib/constants';
 import * as anchor from '@project-serum/anchor';
 
-// we can do this because svektekit/vite allows us to import json file as es modules :)
-import solConfigFile from '../../../../deploy/solana-config.json';
-import solblog_keypair from '../../../../target/deploy/solblog-keypair.json';
 import type { PublicKey } from '$lib/helpers/types'
 import { Connection, PublicKey } from '@solana/web3.js';
 
@@ -14,20 +11,13 @@ import { hash } from "@stablelib/sha256";
 import { sha256 } from 'hash.js'
 import { deserializeUnchecked, Schema } from 'borsh';
 
-const getDevPgmId = () => {
-	// get the program ID from the solblog-keyfile.json
-	let pgmKeypair = anchor.web3.Keypair.fromSecretKey(new Uint8Array(solblog_keypair));
-	return new anchor.web3.PublicKey(pgmKeypair.publicKey); // Address of the deployed program
-};
-
 export const loadAnchorClient = async ({ keypair } = {}) => {
 	if (!!get(anchorClient)) return;
 	let AnchorBlogLibrary = await import('$lib/anchorClient');
 	
-	let config = get(selectedNetwork) == DEV_NET ? solConfigFile.development.config : configMainnet
-	let programId = get(selectedNetwork) == DEV_NET ? getDevPgmId() : programIdMainnet
+	let cfg = config[get(selectedNetwork)]; 
 
-	anchorClient.update((_) => new AnchorBlogLibrary.default({ programId, config, keypair })); // establish our Solana connection & load our little library helpers
+	anchorClient.update((_) => new AnchorBlogLibrary.default({ programId, config: cfg, keypair })); // establish our Solana connection & load our little library helpers
 };
 
 // from line
