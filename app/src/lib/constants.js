6b55efe1ec458491e8be259c9234b4ee60868a96
Doug <douganderson444@gmail.com>
2021-10-07 12:36:31 -0300

    update programId

--- app/src/lib/constants.js
+++ app/src/lib/constants.js
@@ -4,9 +4,10 @@ export const DEV_NET = 'devnet';
 export const MAIN_NET = 'mainnet';
 export const APP_WALLET = true;
 export const ADAPTER = '_adapter_immortaldb';
-export const programIdMainnet = '5JBjXWwqWHwPK1NRgV8d9XQ87wtGDKTmkv4LPZjfZZxa';
-export const configMainnet = {
-	httpUri: 'https://api.mainnet-beta.solana.com'
+export const programId = new PublicKey('BLoG9PtBTkhUbGRAYVe8jzd5ji1G2VPkzwbsRV3bnteD');
+export const config = {
+	[MAIN_NET]: { httpUri: 'https://api.mainnet-beta.solana.com' },
+	[DEV_NET]: { httpUri: 'https://api.devnet.solana.com' }
 };
 
 // for Twitter name service
