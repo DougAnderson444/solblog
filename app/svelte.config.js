452859441fc0ef6eb661aa3e45245f72c78d37f4
Doug <douganderson444@gmail.com>
2021-10-11 18:35:39 -0300

    rm one dir

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-// import adapter_ipfs from 'sveltejs-adapter-ipfs';
+import adapter_ipfs from 'sveltejs-adapter-ipfs';
 import vercelAdapter from '@sveltejs/adapter-vercel';
 import path from 'path';
 
@@ -8,19 +8,19 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		// adapter: adapter_ipfs({
-		// 	removeBuiltInServiceWorkerRegistration: true,
-		// 	injectPagesInServiceWorker: true
-		// }),
-		adapter: vercelAdapter({
-			outfile: '../build'
+		adapter: adapter_ipfs({
+			removeBuiltInServiceWorkerRegistration: true,
+			injectPagesInServiceWorker: true
 		}),
+		// adapter: vercelAdapter({
+		// 	outfile: '../build'
+		// }),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		vite: () => ({
 			resolve: {
 				alias: {
-					$idl: path.resolve('../target/idl/')
+					$idl: path.resolve('./target/idl/')
 				}
 			}
 		})
