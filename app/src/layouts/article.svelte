16693b5ae5c56c21b58957baa7c9b43ff2bffc97
Doug <douganderson444@gmail.com>
2021-10-14 20:42:12 -0300

    add layout

---
+++ app/src/layouts/article.svelte
@@ -0,0 +1,24 @@
+<script>
+	// These props get filled in from the page's front matter
+	export let title;
+	export let coverImageUrl;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta property="og:title" content={title} />
+	<meta property="og:image" content={coverImageUrl} />
+</svelte:head>
+
+<article>
+	<h1>{title}</h1>
+	<slot />
+</article>
+
+<style>
+	article {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
